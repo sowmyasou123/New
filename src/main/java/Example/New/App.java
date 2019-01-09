7878f89e57d724107042c37279c762324c6b4227
SowmyaNY <SowmyaNY@9.102.25.177>
2019-01-09 17:05:06 +0530

    Project New

---
+++ src/main/java/Example/New/App.java
@@ -0,0 +1,13 @@
+package Example.New;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Example/New/NewTest.java
@@ -0,0 +1,10 @@
+package Example.New;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("this is at test");
+  }
+}
